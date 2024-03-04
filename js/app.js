3f06b1a43a0ab9eb12b84a5e1492b3170671b398
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-03-04 14:53:17 +0100

    FEAT - js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,22 @@
 //va applicato uno sconto del 20% per i minorenni
 //oppure applicato uno sconto del 40% per gli over 65.
 //L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, per indicare centesimi sul prezzo. Questo richiederà un minimo di ricerca.
+
+//dichiarazione delle costanti e richiesta tramite prompt
+const km = parseFloat(prompt('Inserisci i chilometri da percorrere:')); //number
+const age = parseInt(prompt('Inserisci la tua età:')); //number
+
+//dichiarazione della variabile prezzo calcolata sulla base dei chilometri da percorrere
+let price = km*0.21; //number
+
+//controllo dell'età per applicare lo sconto
+if (age < 18){
+    price = price - price*0.2; //number
+} 
+if (age >= 65){
+        price = price - price*0.4; //number
+} 
+//dichiarazione della costante numero arrotondato
+const roundPrice = Math.round(price*100) /100;
+
+console.log('Il prezzo del biglietto è: ' + roundPrice);
